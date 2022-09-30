482a5269ecb21a3334d589a8ccb89bcbc52a9095
abhishek.98 <abhishek.verma@planetspark.in>
2022-09-30 20:06:07 +0530

    UI-4: Data and time UI

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import wallpaper from "./assets/images/wallpaper.webp";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={wallpaper} style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons name="ios-lock-closed" size={24} color="white" />
+        <Text style={styles.date}>Friday, 30 September</Text>
+        <Text style={styles.time}>15:26</Text>
+      </View>
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  header: {
+    height: 250,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  date: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#C3FFFE",
+    marginTop: 20,
+  },
+  time: {
+    fontSize: 82,
+    fontWeight: "bold",
+    color: "#C3FFFE",
   },
 });
