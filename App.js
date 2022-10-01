60ced456fdd6c86d4c06eb053e21a6bb885ad214
abhishek.98 <abhishek.verma@planetspark.in>
2022-10-01 14:30:12 +0530

    Hide/show the footer

--- App.js
+++ App.js
@@ -5,11 +5,18 @@ import wallpaper from "./assets/images/wallpaper.webp";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import dayjs from "dayjs";
 import NotificationsList from "./src/components/NotificationsList";
-import Animated, { SlideInDown, SlideInUp } from "react-native-reanimated";
+import Animated, {
+  SlideInDown,
+  SlideInUp,
+  useAnimatedStyle,
+  useSharedValue,
+  interpolate,
+} from "react-native-reanimated";
 import SwipUpToOpen from "./src/components/SwipUpToOpen";
 
 export default function App() {
   const [date, setDate] = useState(dayjs());
+  const footerVisibility = useSharedValue(1);
 
   useEffect(() => {
     let timer = setInterval(() => {
@@ -18,20 +25,29 @@ export default function App() {
 
     return () => clearInterval(timer);
   }, []);
+
+  const animatedFooterStyle = useAnimatedStyle(() => ({
+    marginTop: interpolate(footerVisibility.value, [0, 1], [-85, 0]),
+    opacity: footerVisibility.value,
+  }));
   return (
     <ImageBackground source={wallpaper} style={styles.container}>
       <NotificationsList
+        footerVisibility={footerVisibility}
         ListHeaderComponent={() => (
-          <View style={styles.header}>
+          <Animated.View style={styles.header}>
             <Ionicons name="ios-lock-closed" size={24} color="white" />
 
             <Text style={styles.date}>{date.format("dddd, DD MMMM")}</Text>
             <Text style={styles.time}>{date.format("hh:mm")}</Text>
-          </View>
+          </Animated.View>
         )}
       />
 
-      <Animated.View entering={SlideInDown} style={styles.footer}>
+      <Animated.View
+        entering={SlideInDown}
+        style={[styles.footer, animatedFooterStyle]}
+      >
         <View style={styles.icon}>
           <MaterialCommunityIcons name="flashlight" size={24} color="white" />
         </View>
--- src/components/NotificationsList.js
+++ src/components/NotificationsList.js
@@ -1,19 +1,37 @@
 import { FlatList, useWindowDimensions } from "react-native";
-
 import notifications from "../../assets/data/notifications";
-
 import NotificationItem from "./NotificationItem";
+import Animated, {
+  useAnimatedScrollHandler,
+  withTiming,
+} from "react-native-reanimated";
 
-const NotificationsList = ({ ...flatListProps }) => {
+const NotificationsList = ({ footerVisibility, ...flatListProps }) => {
   const { height } = useWindowDimensions();
 
+  const handler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      const y = event.contentOffset.y;
+
+      if (y < 10) {
+        // footer open
+        footerVisibility.value = withTiming(1);
+      } else {
+        footerVisibility.value = withTiming(0);
+        // footer close
+      }
+    },
+  });
+
   return (
-    <FlatList
+    <Animated.FlatList
       data={notifications}
       renderItem={({ item, index }) => (
         <NotificationItem data={item} index={index} />
       )}
       {...flatListProps}
+      onScroll={handler}
+      scrollEventThrottle={16}
     />
   );
 };
